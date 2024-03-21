336a2eead4b3ba5eef719c453cf89044b0538a68
carlosjau99 <cjaureguil@miumg.edu.gt>
2024-03-20 22:53:08 -0600

    arrays

---
+++ bidimensionall.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+main(){
+	int fil=0,col=0;
+	cout<<"ingrese filas:";
+	cin>>fil;
+	cout<<"ingrese las columnas:";
+	cin>>col;
+	
+	int matriz[fil][col];
+	cout<<"---------ingresar-----------"<<endl;
+	for(int i=0;i<fil;i++){
+		for(int ii=0;ii<col;ii++){
+			cout<<"i"<<","<<ii<<":";
+			cin>>matriz[i][ii];		
+	
+				}
+			cout<<"________"<<endl;	
+	}
+	
+	
+	
+	
+	
+	system("pause");
+}
